e36a07698d54afdb32b35e05c87747ec0a3defb7
Kevin Tarr <kgt2110@columbia.edu>
2016-06-14 11:46:04 -0400

    Xcode project

---
+++ _HTV_VR/_HTV_VR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  _HTV_VR
+//
+//  Created by Gboinyee Tarr on 6/14/16.
+//  Copyright © 2016 Kevin Tarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
